8247d3c76589dd51c15a259c4a7c7be5af05f616
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-05-23 21:37:11 +0300

    test user API add, fronend fetch api, loging works

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 4000;
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: false,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
