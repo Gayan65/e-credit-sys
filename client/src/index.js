5cff8bfb877e69be6ac8204c964dcccfb8c39026
Gayan65 <gayan65728@yahoo.com>
2024-05-23 05:42:13 +0300

    Login component changed, add action function to trigger the credentials

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
-import Login from "./Login";
+import { Login, loginAction } from "./Login";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
 } from "react-router-dom";
 import ErrorPage from "./components/pages/ErrorPage";
 import Nav from "./components/layouts/Nav";
@@ -15,22 +15,22 @@ import Contact from "./components/pages/Contact";
 import { CurrentClients } from "./components/pages/CurrentClients";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Nav />}>
-        <Route index path="/home" element={<CurrentClients />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Route>
-      <Route path="*" element={<ErrorPage />} />
-    </Route>
-  )
+    createRoutesFromElements(
+        <Route>
+            <Route path="/" element={<Login />} action={loginAction} />
+            <Route path="/home" element={<Nav />}>
+                <Route index path="/home" element={<CurrentClients />} />
+                <Route path="about" element={<About />} />
+                <Route path="contact" element={<Contact />} />
+            </Route>
+            <Route path="*" element={<ErrorPage />} />
+        </Route>
+    )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
